8bd494d09dd848daf3844301cae5828cb4861a89
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-08-25 22:26:34 +0100

    display Yavin datat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Starwars from './components/starwars'
 import Pagination from './components/pagination'
-import axios from 'axios'
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
--- src/components/starwars.js
+++ src/components/starwars.js
@@ -14,9 +14,17 @@ function Starwars(){
 
     return (
         <div>
-            <h1>Planet Name = {planet.name}</h1>
-            <h1>rotation period = {planet.rotation_period}</h1>
-            <h1>orbital_period = {planet.orbital_period}</h1>
+            
+    <h1>name: Yavin IV </h1>
+	<h1>rotation_period: 24 </h1>
+	<h1> orbital_period: 4818 </h1>
+	<h1> diameter: 10200 </h1>
+	<h1> climate: temperate, tropical </h1>
+	<h1> gravity: 1 standard </h1>
+	<h1> terrain: jungle, rainforests </h1>
+	<h1> surface_water: 8 </h1>
+	<h1> population: 1000 </h1>
+           
         </div>
     )
 }
