2c15130efaebd8bac2214793ebc094c0f65c02c4
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-09-02 18:22:08 +0100

    Further Css elements

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -4,9 +4,11 @@ import React from 'react'
 function Pagination({ gotoNextPage, gotoPrevPage}) {
     return (
         <div>
+            <h2>
            {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
-           <div class="divider"/>
-           {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+           {/* <div class="divider"/> */} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           {gotoNextPage && <button onClick={gotoNextPage} style={{width : '70px'}}>Next</button>}
+           </h2>
         </div>
     )
 }
